816a3bcbb2a7a2e443fed15cc08afca521b00a67
angelicasanchezbarreto <angelica.sanchez@utec.edu.pe>
2019-11-08 00:15:35 -0500

    Login Activity

--- ChatMobile/app/src/main/java/cs2b01/utec/chat_mobile/LoginActivity.java
+++ ChatMobile/app/src/main/java/cs2b01/utec/chat_mobile/LoginActivity.java
@@ -2,6 +2,7 @@ package cs2b01.utec.chat_mobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void Authenticate(View view){
+
     }
 }
